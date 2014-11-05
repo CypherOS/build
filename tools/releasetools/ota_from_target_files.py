bff973913aaa973990584de842bb6bb1c5daea87
Ricardo Cerqueira <ricardo@cyngn.com>
2014-11-05 19:58:59 +0000

    Fix recovery-script cleanups in block mode
    Change-Id: I4659160800fdbc85eb4478da8dea6a30f5c959ed

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -536,8 +536,12 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   if OPTIONS.backuptool:
     script.ShowProgress(0.02, 10)
+    if OPTIONS.block_based:
+      script.Mount("/system")
     script.RunBackup("restore")
-	
+    if OPTIONS.block_based:
+      script.Unmount("/system")
+
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
 
