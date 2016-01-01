c050acabfe2e8dd843729671daa4cf6a9abcda57
Joe Maples <frap129@gmail.com>
2016-01-01 00:58:35 -0500

    Show compiler used when building
    Some slighly modified logic + updates for nougat.
    
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    
    # Conflicts:
    #	core/definitions.mk

--- core/binary.mk
+++ core/binary.mk
@@ -260,6 +260,13 @@ endif
 my_cppflags := $(my_cpp_std_version) $(my_cppflags)
 
 
+# Export compiler type for display
+ifneq ($(my_clang),true)
+    my_compiler := gcc
+else
+    my_compiler := clang
+endif
+
 # arch-specific static libraries go first so that generic ones can depend on them
 my_static_libraries := $(LOCAL_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_static_libraries)
 my_whole_static_libraries := $(LOCAL_WHOLE_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_WHOLE_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_whole_static_libraries)
