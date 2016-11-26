ca1047a41cc24834405d295cea5e04b682d6c47f
Filipe Carreto <carreto.idn@gmail.com>
2016-11-26 20:42:28 +0000

    Add SuperSU Installer (1/2)
    Change-Id: I9af016293f5dd2949fad28cb327e54ab03fcbd38

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -804,6 +804,13 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   script.ShowProgress(0.05, 5)
   script.WriteRawImage("/boot", "boot.img")
+	
+  if block_based:
+    script.Print("Flashing SuperSU...")
+    common.ZipWriteStr(output_zip, "supersu/supersu.zip",
+                   ""+input_zip.read("SYSTEM/addon.d/UPDATE-SuperSU.zip"))
+    script.Mount("/system")
+    script.FlashSuperSU()
 
   script.ShowProgress(0.2, 10)
   device_specific.FullOTA_InstallEnd()
