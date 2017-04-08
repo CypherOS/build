56233b7d0eaf8db3317e296703083bb76412a75b
Chris <chris@cypheros.co>
2017-04-08 14:16:50 -0400

    core: Compilation fixes after merge
    Give LOCAL_IGNORE_SUBDIR it's own if statement, otherwise a filtered
    statement will cause framework dep errors
    
    Change-Id: I1d84e0668433b12f22f60b4d984687c6e1088e4c

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -210,8 +210,9 @@ ifneq (true,$(LOCAL_UNINSTALLABLE_MODULE))
   # Apk and its attachments reside in its own subdir.
   ifeq ($(LOCAL_MODULE_CLASS),APPS)
   # framework-res.apk doesn't like the additional layer.
-  ifeq ($(filter true,$(LOCAL_NO_STANDARD_LIBRARIES) $(LOCAL_IGNORE_SUBDIR)),)
+  ifeq ($(LOCAL_NO_STANDARD_LIBRARIES),true)
   # Neither do Runtime Resource Overlay apks, which contain just the overlaid resources.
+  else ifeq ($(LOCAL_IGNORE_SUBDIR),true)
   else ifeq ($(LOCAL_IS_RUNTIME_RESOURCE_OVERLAY),true)
   else
     my_module_path := $(my_module_path)/$(LOCAL_MODULE)
