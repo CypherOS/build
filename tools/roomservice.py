c16fc2c7a6f7feb19ed7647419a8526bcb5ff5b2
Chris <chris@cypheros.co>
2017-05-31 19:26:05 -0400

    roomservice: Update default branch to 'nougat-mr2'
    Change-Id: I804965d685f794db28aba89a2987834e0af44e5e

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -126,7 +126,7 @@ def reposync(syncrepo):
 def add_to_local_manifest(path, name, remote, branch=None):
     if (remote == "aoscp"):
         if (branch == None):
-            branch = "nougat-mr1"
+            branch = "nougat-mr2"
 
     if is_path_in_manifest(path, name, remote, branch):
         # Error messages are present in the called function, so just exit
