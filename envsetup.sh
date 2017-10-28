b697597c52dea91ba9d50a5aa3e8b94b56d3c29f
Luca Stefani <luca020400@lineageos.org>
2017-10-27 22:41:26 -0300

    envsetup: Update mka for O
    Change-Id: I36ec10be9152f135ec3645b4304e63ea30d06774

--- envsetup.sh
+++ envsetup.sh
@@ -1611,20 +1611,14 @@ function godir () {
 }
 
 function mka() {
-    local T=$(gettop)
-    if [ "$T" ]; then
-        case `uname -s` in
-            Darwin)
-                make -C $T -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
-                ;;
-            *)
-                mk_timer schedtool -B -n 1 -e ionice -n 1 make -C $T -j$(cat /proc/cpuinfo | grep "^processor" | wc -l) "$@"
-                ;;
-        esac
-
-    else
-        echo "Couldn't locate the top of the tree.  Try setting TOP."
-    fi
+    case `uname -s` in
+        Darwin)
+            m -j "$@"
+            ;;
+        *)
+            mk_timer schedtool -B -n 10 -e ionice -n 7 m -j "$@"
+            ;;
+    esac
 }
 
 # Force JAVA_HOME to point to java 1.7/1.8 if it isn't already set.
