6dc49bfe648366dede3621fd292519be26fd1c70
Chris <chris@cypheros.co>
2017-12-08 19:35:33 -0500

    core: Include sepolicy vendor extension

--- core/config.mk
+++ core/config.mk
@@ -965,7 +965,7 @@ endif
 ifneq ($(AOSCP_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-$(eval include vendor/aoscp/sepolicy/sepolicy.mk)
+$(eval include vendor/aoscp-sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
