a6c9d08f34897605a794ac0a0c6bf63f2ff3c62f
Luca Stefani <luca020400@lineageos.org>
2018-01-23 03:01:50 -0500

    core: Include QCOM/MTK targets
    Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7

--- core/config.mk
+++ core/config.mk
@@ -971,6 +971,12 @@ $(eval include vendor/aoscp-sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/aoscp-build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/aoscp-build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk
