b8218112b2ca450d4be4bcec6339f293f86b5667
Luca Stefani <luca020400@lineageos.org>
2018-01-23 02:58:25 -0500

    build: Implementation for aoscp build environment
    core: Move aoscp sepolicy to vendor/aoscp-sepolicy
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357

--- envsetup.sh
+++ envsetup.sh
@@ -135,6 +135,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^aoscp_") ; then
+        AOSCP_BUILD=$(echo -n $1 | sed -e 's/^aoscp_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $AOSCP_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+    else
+        AOSCP_BUILD=
+    fi
+    export AOSCP_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -613,6 +621,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
