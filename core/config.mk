6dddbfeb0dad4cb63b85639055f22319dc58e20d
Chris <chris@cypheros.co>
2018-08-11 22:34:20 -0400

    core: Include aoscp BoardConfig is available
    We do not have this at the moment, but may add it later on.
    To prevent build failure, be sure it's included if it exists
    
    Change-Id: I85378eeccead64deee4b19fc33a1186dae44a0a8

--- core/config.mk
+++ core/config.mk
@@ -230,7 +230,7 @@ $(call project-set-path-variant,ril,TARGET_RIL_VARIANT,hardware/ril)
 
 -include vendor/extra/BoardConfigExtra.mk
 ifneq ($(AOSCP_BUILD),)
-include vendor/aoscp/configs/BoardConfigAOSCP.mk
+-include vendor/aoscp/configs/BoardConfigAOSCP.mk
 endif
 
 # The build system exposes several variables for where to find the kernel
